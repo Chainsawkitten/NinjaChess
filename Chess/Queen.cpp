f2244b889466e261c1fb986f518997798b2a3294
Daniel Pejcinovski <danielpejcinovski@hotmail.com>
2015-05-13 14:48:13 +0200

    Queen moves added.

--- Chess/Queen.cpp
+++ Chess/Queen.cpp
@@ -1,8 +1,67 @@
 #include "Queen.h"
+#include "Board.h"
 
 namespace Chess {
 	Queen::Queen(const Position& position, bool white) : Piece(position, white) {
 
+	}
+
+	std::vector<Position> Queen::legalMoves(const Board& board) const {
+		std::vector<Position> moves;
+
+		for (int i = -1; i < 2; i += 2) {
+			for (int j = -1; j < 2; j += 2) {
+				int tempX = i;
+				int tempY = j;
+				while (true) {
+					Position newPos = position;
+					newPos.x += tempX;
+					newPos.y += tempY;
+					tempX += i;
+					tempY += j;
+
+					Piece* temp = board.getPiece(newPos);
+					if (temp == nullptr && newPos.valid()) {
+						moves.push_back(newPos);
+					}
+					else if (!newPos.valid() || isWhite() == temp->isWhite()) {
+						break;
+					}
+					else{
+						moves.push_back(newPos);
+						break;
+					}
+				}
+			}
+		}
+
+		for (int i = 0; i < 4; i++) {
+			int num = i % 2 == 0 ? position.x : position.y;
+			int way = 1 + (-2 * (i < 2));
+
+			for (int j = num + way; j >= 0 && j <= 7; j += way) {
+				Position newPos = position;
+				if (i % 2 == 0) {
+					newPos.x = j;
+				}
+				else {
+					newPos.y = j;
+				}
+
+				Piece* other = board.getPiece(newPos);
+				if (other == nullptr) {
+					moves.push_back(newPos);
+				}
+				else if (other->isWhite() == isWhite()) {
+					break;
+				}
+				else {
+					moves.push_back(newPos);
+					break;
+				}
+			}
+		}
+		return moves;
 	}
 
 	char Queen::notation() const {
